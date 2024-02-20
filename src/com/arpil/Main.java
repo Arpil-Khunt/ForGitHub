a2516384b59aa327f766674ddfb8f1c0a9e294ac
arpil_khunt <137201938+Arpil-Khunt@users.noreply.github.com>
2024-02-20 18:35:09 +0530

    Update Main.java
    Trying to update from remote repository to local repository

--- src/com/arpil/Main.java
+++ src/com/arpil/Main.java
@@ -2,7 +2,9 @@ package com.arpil;
 
 public class Main {
     public static void main(String args[]){
+        
         System.out.println("Trying for GitHub");
+        System.out.println("Hello, I am trying to changes from GitHub");
         System.out.println("Again Trying");
     }
 }
