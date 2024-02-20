13e049121331cd6044c4062f1d3ccfafec9045d8
arpil_khunt <arpilkhunt1203@gmail.com>
2024-02-20 18:29:16 +0530

    Changes added in Main and Try file

--- src/com/arpil/Main.java
+++ src/com/arpil/Main.java
@@ -2,6 +2,7 @@ package com.arpil;
 
 public class Main {
     public static void main(String args[]){
-        System.out.println("Trying for Git");
+        System.out.println("Trying for GitHub");
+        System.out.println("Again Trying");
     }
 }
--- src/com/arpil/Try.java
+++ src/com/arpil/Try.java
@@ -3,5 +3,8 @@ package com.arpil;
 public class Try {
     public static void main(String args[]){
         System.out.println("Hello world!!!");
+        System.out.println("hello");
+        System.out.println("Bonjour");
+        System.out.println("Namaste");
     }
 }
